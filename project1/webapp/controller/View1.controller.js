8cb5a18bb24282ef3966d534e4d47c941085e3e8
Sharan N <sharan.n@asint.net>
2026-04-15 20:14:44 +0530

    Base commit

---
+++ project1/webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.asint.rebase.merge.project1.controller.View1", {
+        onInit() {
+        }
+    });
+});
